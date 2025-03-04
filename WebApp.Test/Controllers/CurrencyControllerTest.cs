0fcc121d012e38f71f0e3ae1699df514ecbf2c87
basvarajjm <40737198+basvarajjm@users.noreply.github.com>
2025-03-04 23:11:19 +0530

    Added test project

---
+++ WebApp.Test/Controllers/CurrencyControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WebApp.Controllers;
+using WebApp.Interfaces;
+using WebApp.Models;
+
+namespace WebApp.Test.Controllers
+{
+    public class CurrencyControllerTest
+    {
+        private readonly Mock<ICurrenyConversionService> _currenyConversionService;
+        private readonly Mock<ILogger<CurrencyController>> _logger;
+        private readonly CurrencyController _currencyController;
+
+        public CurrencyControllerTest()
+        {
+            _logger = new Mock<ILogger<CurrencyController>>();
+            _currenyConversionService = new Mock<ICurrenyConversionService>();
+            _currencyController = new CurrencyController(_logger.Object, _currenyConversionService.Object);
+        }
+
+        [Theory]
+        [InlineData("", 0)]
+        public async Task GetDKKEquivalent_ShouldReturnBadRequestWhenDataIsNotProvided(string currency, long value)
+        {
+            // Arrange
+
+            // Act
+            var response = await _currencyController.GetDKKEquivalent(currency, value);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(response);
+        }
+
+        [Theory]
+        [InlineData("USD", 20, 0)]
+        public async Task GetDKKEquivalent_ShouldReturnResponseWhenDataIsProvided(string currency, long value, double output)
+        {
+            // Arrange
+            _currenyConversionService.Setup((c) => c.GetDKKEquivalentOf(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<CancellationToken>())).ReturnsAsync(output);
+
+            // Act
+            var response = await _currencyController.GetDKKEquivalent(currency, value);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(response);
+        }
+
+        [Fact]
+        public async Task GetAllCurrencyRates_ShouldReturnList()
+        {
+            var list = new List<Currency>();
+            // Arrange
+            _currenyConversionService.Setup(c => c.GetAllCurrenciesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(list);
+
+            // Act
+            var response = await _currencyController.GetAllCurrencyRates();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(response);
+        }
+    }
+}
---
+++ WebApp.Test/Services/CurrencyConversionServiceTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WebApp.Exceptions;
+using WebApp.Interfaces;
+using WebApp.Models;
+using WebApp.Repositories;
+using WebApp.Services;
+
+namespace WebApp.Test.Services
+{
+    public class CurrencyConversionServiceTest
+    {
+        private readonly Mock<ILogger<CurrencyConversionService>> _logger;
+        private readonly Mock<IDKBankService> _dKBankService;
+        private readonly CurrencyConversionService _currencyConversionService;
+
+        public CurrencyConversionServiceTest()
+        {
+            _logger = new Mock<ILogger<CurrencyConversionService>>();
+            _dKBankService = new Mock<IDKBankService>();
+
+            var mem = new Mock<IMemoryCache>();
+            var cacheRepository = new CacheRepository(mem.Object);
+            
+            var fileLogger = new Mock<ILogger<FileRepository>>();
+            var fileRepository = new FileRepository(fileLogger.Object);
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddMemoryCache();
+            serviceCollection.AddSingleton(cacheRepository);
+            serviceCollection.AddSingleton(fileRepository);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            _currencyConversionService = new CurrencyConversionService(_logger.Object, serviceProvider, _dKBankService.Object);
+        }
+
+        [Theory]
+        [InlineData("ABC", 10)]
+        public async Task GetDKKEquivalentOf_ShouldReturnItemNotFoundExceptionWhenCurrencyIsInvalid(string currency, long value)
+        {
+            // Arrange
+            CancellationToken cancellationToken = default;
+
+            // Act
+            try
+            {
+                var result = await _currencyConversionService.GetDKKEquivalentOf(currency, value, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.IsType<ItemNotFoundException>(ex);
+            }
+        }
+
+        [Theory]
+        [InlineData("USD", 10)]
+        public async Task GetDKKEquivalentOf_ShouldReturnDataWhenCurrencyIsValid(string currency, long value)
+        {
+            // Arrange
+            CancellationToken cancellationToken = default;
+
+            // Act
+            double result = 0;
+            try
+            {
+                result = await _currencyConversionService.GetDKKEquivalentOf(currency, value, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.Fail("Should not throw any exception.");
+            }
+            Assert.IsType<double>(result);
+        }
+    }
+}
